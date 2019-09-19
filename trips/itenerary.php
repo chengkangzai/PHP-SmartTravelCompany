eea742dd1a1d6cb2954c1e1771fb86be535d714e
chengkangzai <pycck@hotmail.com>
2019-09-19 11:22:23 +0800

    update image

--- trips/itenerary.php
+++ trips/itenerary.php
@@ -1,6 +1,13 @@
 <?php
-include(../config.php);
-$itenerary_sql="SELECT * FROM Tour"
+include('../config.php');
 
-$itenerary_row=mysqli_
+$itenerary_sql="SELECT * FROM Tour Inner join Trip On Trip.FK_TourCode=Tour.TourCode";
+
+$itenerary_query=mysqli_query($db,$itenerary_sql);
+
+$itenerary_row=mysqli_fetch_array($itenerary_sql,MYSQLI_ASSOC);
+
+
+
+
 ?>
