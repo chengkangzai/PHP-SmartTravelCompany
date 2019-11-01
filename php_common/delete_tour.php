51bf67030c3085325f67edcb842cb649c03ee2c7
chengkangzai <pycck@hotmail.com>
2019-11-01 15:20:21 +0800

    chage the SQL process sequence

--- php_common/delete_tour.php
+++ php_common/delete_tour.php
@@ -1,34 +1,27 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     include_once("../config.php");
-
     $TourCode = mysqli_real_escape_string($db, $_POST['TourCode']);
-
-
-
-
 
     $d_tour_sql = "DELETE FROM `Tour` WHERE`TourCode`='$TourCode'";
     $d_tourdes_sql = "DELETE FROM `Tour_des` WHERE`FK_TourCode`='$TourCode'";
     $d_selected_tour_sql = "DELETE FROM `C_selected_Tour` WHERE`FK_TourCode`='$TourCode'";
 
-        
-
-
     if (mysqli_query($db, $d_tourdes_sql)) {
-        if (mysqli_query($db, $d_tour_sql)) {
-            if (mysqli_query($db, $d_selected_tour_sql)) {
-                echo ("<script> alert('Edit Sucess!'); </script>");
+        if (mysqli_query($db, $d_selected_tour_sql)) {
+            if (mysqli_query($db, $d_tour_sql)) {
+                echo ("<script> alert('Delete Sucess!'); </script>");
                 echo ("<script> window.history.go(-1)');</script>");
             }else {
-                echo ("<script> alert('unable to delete C_Selected_Tour'); </script>");
-                echo ("<script> window.history.go(-1)');</script>");
+                header("Location:../jump/reject_delete_tour.html");
             }
         }else {
-            header("Location:../jump/reject_delete_tour.html");
+            echo ("<script> alert('unable to delete C_Selected_Tour'); </script>");
+            echo ("<script> window.history.go(-1)');</script>");
         }
     } else {
         echo ("<script> alert('unable to delete Tour_des'); </script>");
         echo ("<script> window.history.go(-1)');</script>");
     }
 }
+?>
