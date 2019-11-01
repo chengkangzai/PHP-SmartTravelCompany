3017def011e7b435fa93a318237d494ce7749a0b
chengkangzai <pycck@hotmail.com>
2019-11-01 13:33:35 +0800

    option

--- php_common/delete_trip.php
+++ php_common/delete_trip.php
@@ -1,13 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST" ) {
-    
     include_once("../config.php");
-
     $Trip_ID = mysqli_real_escape_string($db, $_POST['Trip_ID']);
     $sql = "DELETE FROM `Trip` WHERE `Trip_ID`='$Trip_ID' ";
-
-
-
     if (mysqli_query($db, $sql)) {
         echo ("<script> alert('Delete Sucess!'); </script>");
         echo ("<script> window.history.go(-1)');</script>");
@@ -15,6 +10,5 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" ) {
         echo ("<script> alert('You Are Deleting a trip that already booked!'); </script>");
         echo ("<script> window.history.go(-1)');</script>");
     }
-
 }
 ?>
--- welcome.php
+++ welcome.php
@@ -1424,14 +1424,14 @@ if ($_SESSION['role']=="Customer") {
         
     </script>
 
-    <script>
-        function dconfirm() {
-            var r = confirm("You are about to delete a record ! \n Are you sure ?");
-            if (r == FALSE) {
-                location.reload();
-            }
-        }
-    </script>
+<script>
+function dconfirm() {
+    var r = confirm("You are about to delete a record ! \n Are you sure ?");
+    if (r == FALSE) {
+        location.reload();
+    }
+}
+</script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
