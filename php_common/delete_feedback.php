0c1455b3142dd42a36a5bd91c2e5d9b07a4b33e4
chengkangzai <pycck@hotmail.com>
2020-01-19 18:04:06 +0800

    Verify the people

---
+++ php_common/delete_feedback.php
@@ -0,0 +1,16 @@
+<?php
+include("../config.php");
+include("../session.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($position == "Manager" || $position == "Assistant Manager") {
+        $id = mysqli_real_escape_string($db, $_POST['id']);
+
+        $sql = "DELETE FROM Feedback WHERE Feedback_ID=$id";
+
+        if (mysqli_query($db, $sql)) {
+            echo "success";
+        }
+    }else{
+        echo "No permission ";
+    }
+}
