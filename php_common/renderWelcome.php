f07f35e571ba255b1b942936816dea735d3130a5
chengkangzai <pycck@hotmail.com>
2020-02-02 09:58:13 +0800

    :bug: fix logical error :fire: filter managed trip

--- php_common/renderWelcome.php
+++ php_common/renderWelcome.php
@@ -125,11 +125,9 @@ function renderManagedTrip()
     $today = date("Y-m-d");
     $sql="SELECT B.Booking_ID,C.FName,C.LName,Trip.Trip_ID,T.TourCode,C.Phone_num,T.Name,T.Destination,Trip.Departure_date,Trip.Fee,Trip.Airline,E.username ,T.itinerary_url,C.username FROM Booking B INNER JOIN Customer C ON B.FK_C_username=C.username INNER JOIN Trip on Trip.Trip_ID=B.FK_Trip_ID INNER join Tour T on Trip.FK_TourCode=T.TourCode INNER JOIN Employee E on T.FK_E_username=E.username ";
 
-    $outputTable="<tr ><td >{$row['FName']} {$row['LName']}</td><td id='customerPhone$ran' data-id='{$row['username']}'>{$row['Phone_num']}</td><td>{$row['TourCode']}</td><td>{$row['Name']}</td><td>{$row['Destination']}</td><td>{$row['Departure_date']}</td><td>RM {$row['Fee']}</td><td>{$row['Airline']}</td><td><a href='{$row[' itinerary_url']}'><img src='img/itenerary-dark.png' /></a> </td><td><a class='btn btn-primary text-white' role='button' onclick='makeUpdate(\"$ran\")' id='btn_$ran'>Update</a></td></tr>";
 
     switch ($GLOBALS['position']) {
         case 'Manager':
-            echo $table;
             $whereClause="WHERE Trip.Departure_date >= '$today'";
             break;
 
@@ -142,38 +140,39 @@ function renderManagedTrip()
             $whereClause="WHERE (E.username='$login_session') AND (Trip.Departure_date >= '$today') ";
         break;
     }
-    echo $GLOBALS['table'];
+    echo $table;
     $query_sql = mysqli_query($GLOBALS['db'], $sql.$whereClause);
     while ($row = mysqli_fetch_assoc($query_sql)) {
         $ran = rand();
-        echo $outputTable;
+        echo "<tr ><td >{$row['FName']} {$row['LName']}</td><td id='customerPhone$ran' data-id='{$row['username']}'>{$row['Phone_num']}</td><td>{$row['TourCode']}</td><td>{$row['Name']}</td><td>{$row['Destination']}</td><td>{$row['Departure_date']}</td><td>RM {$row['Fee']}</td><td>{$row['Airline']}</td><td><a href='{$row[' itinerary_url']}'><img src='img/itenerary-dark.png' /></a> </td><td><a class='btn btn-primary text-white' role='button' onclick='makeUpdate(\"$ran\")' id='btn_$ran'>Update</a></td></tr>";
     }
     echo "</tbody></table>";
 }
 
 function renderTripManagement()
 {
+    $date = date("Y-m-d");
     $table = " <table border='0' class='table table-striped table-hover' id='TripTable'> <thead> <tr> <th scope='col'> Trip ID </th> <th scope='col'> Departure Date </th> <th scope='col'> Fee(RM) </th> <th scope='col'> Airline </th> <th scope='col'> Tour Code</th> <th scope='col'> Tour Name</th> <th scope='col'> Update </th> <th scope='col'> Delete </th> </tr> </thead><tbody> ";
     switch ($GLOBALS['position']) {
         case 'Manager':
-            $area = "";
+            $area = "WHERE Tr.Departure_date>='$date'";
             break;
 
         case 'Tour Manager of Europe':
-            $area = "WHERE Category =\'Europe\'";
+            $area = "WHERE (Category ='Europe') AND (Tr.Departure_date >='$date')";
             break;
 
         case 'Tour Manager of Asia':
-            $area = "WHERE Category =\'Asia\'";
+            $area = "WHERE (Category ='Asia') AND (Tr.Departure_date >='$date')";
             break;
 
         case 'Tour Manager of Exotic':
-            $area = "WHERE Category =\'Exotic\'";
+            $area = "WHERE (Category ='Exotic') AND (Tr.Departure_date >='$date')";
             break;
 
         default:
             $login_session = $GLOBALS['login_session'];
-            $area = "WHERE FK_E_username='$login_session'";
+            $area = "WHERE (FK_E_username='$login_session') AND (Tr.Departure_date >='$date')";
             break;
     }
 
